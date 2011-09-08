ab923a92bc7cd5da6c2ea94f5146ce5f537ae050
Kamil Politowicz <k.politowicz@gmail.com>
2011-09-08 17:59:56 +0200

    merged PaBLoX-CL's SCSS changes + made layout generator more flexible but Rails 3.1+
    - you can now specify layout name AND stylesheet name (see USAGE)
    - you can now specify combination of options: --erb or --haml and --css or --sass or --scss (see USAGE)
    - default stylesheet file name is changed to 'nifty_layout'; the rest of defaults remain unchanged (see USAGE and layout features)
    - gem is now 3.1+ -> bundler does not allow to specify rails-3.1 and rspec-2.6.0 dependency
    
    Squashed commit of the following:
    
    commit 1947f4a3609515f4403fae96bac42e7bfb282bdf
    Author: Kamil Politowicz <k.politowicz@gmail.com>
    Date:   Thu Sep 8 17:53:30 2011 +0200
    
        changed layout generator and templates to accept stysheet file name and additional options
    
    commit d65d61b8b5d0d7ce69c849adeaef35dac4fb73c9
    Author: Kamil Politowicz <k.politowicz@gmail.com>
    Date:   Thu Sep 8 17:12:49 2011 +0200
    
        specified the desired options and layout/stylesheet naming
    
        -> getting rid of rails 3.0 expectations - it is 3.1+ gem
    
    commit 8611c19648699ea4fca9b5cb6af021a4519bc9b6
    Author: Kamil Politowicz <k.politowicz@gmail.com>
    Date:   Thu Sep 8 16:46:55 2011 +0200
    
        changed USAGE to describe the desired behavior on rails 3.1
    
    commit dccc961ddfdf2b1809224a1201f7dd6b7095f157
    Author: Kamil Politowicz <k.politowicz@gmail.com>
    Date:   Thu Sep 8 16:19:46 2011 +0200
    
        merged PaBLoX-CL's changes related to rails 3.1 layout generation
    
        Squashed commit of the following:
    
        commit 8ee7978d0bf1bc114b4d4ece089e428a49052066
        Author: Pablo Olmos de Aguilera Corradini <pablo@glatelier.org>
        Date:   Sat Sep 3 16:39:15 2011 -0300
    
            Add the corresponding feature to the change in the directory structure
    
            I tried to keep both versions (3.0.x and 3.1.x) on different gemsets
            using rvm, but for some reason I couldn't. They kept going back to the main
            gemset and being installed there. So, if Rails >=3.1.0 isn't installed
            the nifty_layout_3.1 feature is going to fail.
    
        commit 38099697704b6eb8cade9fb901acfde2ad6381a8
        Author: Maciej Litwiniuk <maciej@galdomedia.pl>
        Date:   Wed Jul 20 17:48:34 2011 +0200
    
            copies assets to Rails 3.1 specific directory (if on Rails 3.1), does not overwrite application.css on Rails 3.1
    
        commit 590eda638f8f25eb624e67bccaf976d6c3c5b38a
        Merge: 3b6a027 c54c47e
        Author: Pablo Olmos de Aguilera Corradini <pablo@glatelier.org>
        Date:   Fri Jun 24 18:33:21 2011 -0700
    
            Merge pull request #1 from igorbozato/patch-1
    
            More readable scss template
    
        commit c54c47eb53bc96aacdcfb94a8e4b6fd1ee62cfe1
        Author: Igor Bozato <igor.hrk@gmail.com>
        Date:   Fri Jun 24 16:24:31 2011 -0700
    
            Just keep it readable
    
        commit 3b6a027aa50b046c63703327d39fade3edab3c6e
        Author: Pablo Olmos de Aguilera <pablo@glatelier.org>
        Date:   Sat Jun 18 21:48:08 2011 -0400
    
            Clean up to nifty_layout cucumber feature
    
        commit 498f01ce79df09dd775496cd9c740576be700226
        Author: Pablo Olmos de Aguilera <pablo@glatelier.org>
        Date:   Sat Jun 18 18:28:44 2011 -0400
    
            Add scss syntax option and modify directory estructure
    
        commit 1efc2c0b6274dd51a6f094e624fe52c3ff51961c
        Author: Pablo Olmos de Aguilera <pablo@glatelier.org>
        Date:   Sat Jun 18 18:24:41 2011 -0400
    
            Modify cucumber features to reflect the new estructure
    
        commit 7778493cc4b4f1c1ffb3c1482444e76f68b6ffd0
        Author: Pablo Olmos de Aguilera <pablo@glatelier.org>
        Date:   Sat Jun 11 23:54:31 2011 -0400
    
            Change the stylesheet directory to be compatible with the new Rails 3.1 structure
    
        commit 70d08a992c81fcda793fe8b110b557963d9810f9
        Author: Pablo Olmos de Aguilera <pablo@glatelier.org>
        Date:   Sat Jun 11 23:44:58 2011 -0400
    
            Add a template with the new syntax

--- features/nifty_layout.feature
+++ features/nifty_layout.feature
@@ -3,18 +3,35 @@ Feature: Nifty Layout Generator
   As a rails developer
   I want to generate a simple layout
 
-  Scenario: Generate normal application layout
+  Background: Create a new rails app
     Given a new Rails app
-    When I run "rails g nifty:layout -f"
-    Then I should see "stylesheet_link_tag "application"" in file "app/views/layouts/application.html.erb"
-    Then I should see "h(page_title" in file "app/helpers/layout_helper.rb"
-    And I should see file "app/helpers/layout_helper.rb"
-    And I should see file "app/helpers/error_messages_helper.rb"
-    And I should see file "public/stylesheets/application.css"
 
-  Scenario: Generate named layout with haml and sass
-    Given a new Rails app
-    When I run "rails g nifty:layout FooBar --haml -f"
-    Then I should see "stylesheet_link_tag "foo_bar"" in file "app/views/layouts/foo_bar.html.haml"
-    And I should see file "public/stylesheets/sass/foo_bar.sass"
+  Scenario: Generate normal application layout
+    When I run "rails g nifty:layout -f"
+    Then I should see "stylesheet_link_tag "nifty_layout"" in file "app/views/layouts/application.html.erb"
+    Then I should see "h(page_title" in file "app/helpers/layout_helper.rb"
+    And I should see the following files
+      | app/helpers/layout_helper.rb            |
+      | app/helpers/error_messages_helper.rb    |
+      | app/assets/stylesheets/nifty_layout.css |
+
+  Scenario: Generate named layout without stylesheet name
+    When I run "rails g nifty:layout FooBar -f"
+    Then I should see "stylesheet_link_tag "foo_bar"" in file "app/views/layouts/foo_bar.html.erb"
+    And I should see the following files
+      | app/helpers/layout_helper.rb            |
+      | app/helpers/error_messages_helper.rb    |
+      | app/assets/stylesheets/foo_bar.css      |
+
+  Scenario Outline: Generate named layout and stylesheet with options
+    When I run "rails g nifty:layout FooBar NiftyStyle <options> -f"
+    Then I should see "stylesheet_link_tag "nifty_style" in file "app/views/layouts/foo_bar.html.<layout_version>"
+    And I should see file "app/assets/stylesheets/nifty_style.<stylesheet_version>"
     And I should see file "app/helpers/layout_helper.rb"
+
+    Scenarios:
+      | options       | layout_version | stylesheet_version |
+      | --erb --sass  | erb            | sass               |
+      | --haml        | haml           | sass               |
+      | --scss        | erb            | css.scss           |
+      | --haml --scss | haml           | css.scss           |
--- lib/generators/nifty/layout/layout_generator.rb
+++ lib/generators/nifty/layout/layout_generator.rb
@@ -3,17 +3,41 @@ require 'generators/nifty'
 module Nifty
   module Generators
     class LayoutGenerator < Base
-      argument :layout_name, :type => :string, :default => 'application', :banner => 'layout_name'
+      DEFAULT_LAYOUT = 'application'
+      DEFAULT_STYLE  = 'nifty_layout'
 
-      class_option :haml, :desc => 'Generate HAML for view, and SASS for stylesheet.', :type => :boolean
+      argument :layout_name,     :type => :string, :default => DEFAULT_LAYOUT,  :banner => 'layout_name'
+      argument :stylesheet_name, :type => :string, :default => DEFAULT_STYLE, :banner => 'stylesheet_name'
+
+      class_option :erb,  :desc => 'Generate ERB  for view (default).',                 :type => :boolean
+      class_option :haml, :desc => 'Generate HAML for view.',                           :type => :boolean
+      class_option :css,  :desc => 'Generate CSS  for stylesheet (default for ERB).',   :type => :boolean
+      class_option :sass, :desc => 'Generate SASS for stylesheet (default for HAML).',  :type => :boolean
+      class_option :scss, :desc => 'Generate SCSS for stylesheet.',                     :type => :boolean
 
       def create_layout
         if options.haml?
-          template 'layout.html.haml', "app/views/layouts/#{file_name}.html.haml"
-          copy_file 'stylesheet.sass', "public/stylesheets/sass/#{file_name}.sass"
-        else
-          template 'layout.html.erb', "app/views/layouts/#{file_name}.html.erb"
-          copy_file 'stylesheet.css', "public/stylesheets/#{file_name}.css"
+          if options.css?
+            template 'layout.html.haml', "app/views/layouts/#{file_name}.html.haml"
+            copy_file 'stylesheet.css', "app/assets/stylesheets/#{style_name}.css"
+          elsif options.scss?
+            template 'layout.html.haml', "app/views/layouts/#{file_name}.html.haml"
+            copy_file 'stylesheet.css.scss', "app/assets/stylesheets/#{style_name}.css.scss"
+          else # --sass
+            template 'layout.html.haml', "app/views/layouts/#{file_name}.html.haml"
+            copy_file 'stylesheet.sass', "app/assets/stylesheets/#{style_name}.sass"
+          end
+        else # --erb
+          if options.sass?
+            template 'layout.html.erb', "app/views/layouts/#{file_name}.html.erb"
+            copy_file 'stylesheet.sass', "app/assets/stylesheets/#{style_name}.sass"
+          elsif options.scss?
+            template 'layout.html.erb', "app/views/layouts/#{file_name}.html.erb"
+            copy_file 'stylesheet.css', "app/assets/stylesheets/#{style_name}.css.scss"
+          else # --css
+            template 'layout.html.erb', "app/views/layouts/#{file_name}.html.erb"
+            copy_file 'stylesheet.css', "app/assets/stylesheets/#{style_name}.css"
+          end
         end
         copy_file 'layout_helper.rb', 'app/helpers/layout_helper.rb'
         copy_file 'error_messages_helper.rb', 'app/helpers/error_messages_helper.rb'
@@ -23,6 +47,14 @@ module Nifty
 
       def file_name
         layout_name.underscore
+      end
+
+      def style_name
+        if layout_name != DEFAULT_LAYOUT && stylesheet_name == DEFAULT_STYLE
+          file_name
+        else
+          stylesheet_name.underscore
+        end
       end
     end
   end
